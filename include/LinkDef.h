d8621ccc50f4d2d63c4f9ac21e966f259cdfdd86
Riccardo Di Sipio <riccardo.di.sipio@cern.ch>
2017-08-15 03:15:36 +0200

    works with python

---
+++ include/LinkDef.h
@@ -0,0 +1,14 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+//#pragma link C++ defined_in "include/EikosUnfolder.h"
+
+#pragma link C++ class Sample;
+#pragma	link C++ class Systematic;
+#pragma link C++ class EikosUnfolder;
+
+#endif
+
