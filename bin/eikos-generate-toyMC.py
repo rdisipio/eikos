96e2ef5d5ca707265dd157708466bf47dcc8bbfd
Riccardo Di Sipio <riccardo.di.sipio@cern.ch>
2018-07-16 16:15:52 -0400

    commit

---
+++ bin/eikos-generate-toyMC.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import os, sys
+from ROOT import *
+
+
--- bin/eikos-setenv.sh
+++ bin/eikos-setenv.sh
@@ -3,3 +3,5 @@ export PYTHONPATH=$PWD/python:$PYTHONPATH
 
 [ ! -d output ] && mkdir -p output
 [ ! -d output/img ] && mkdir -p output/img
+
+lsetup "lcgenv -p LCG_93 x86_64-centos7-gcc62-opt ROOT"
